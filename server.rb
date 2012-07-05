7f9a64b1a35571cdc66d0f499db5b28f39850f7c
Raj Shah <brainix@gmail.com>
2012-07-04 21:38:03 -0700

    refactored code

--- server.rb
+++ server.rb
@@ -6,6 +6,17 @@ require 'json'
 require 'net/http'
 
 
+def report_install_to_crowdmob
+  hashed_mac_address = hash_mac_address('salt', '11:11:11:11:11:11')
+  secret_hash = compute_secret_hash('5bb75e8dd6300cadcdd07fa2c46a3c10', 'lulzio', hashed_mac_address)
+  response, data = issue_post_request('lulzio', hashed_mac_address, secret_hash)
+  json = JSON.parse(response.body)
+
+  puts "HTTP status code: #{response.code}"
+  puts "CrowdMob internal status code: #{json['install_status']}"
+end
+
+
 def hash_mac_address(salt, mac_address)
   Digest::SHA2.hexdigest(salt + mac_address)
 end
@@ -16,7 +27,7 @@ def compute_secret_hash(app_secret_key, app_permalink, hashed_mac_address)
 end
 
 
-def report_install_to_crowdmob(app_permalink, hashed_mac_address, secret_hash)
+def issue_post_request(app_permalink, hashed_mac_address, secret_hash)
   post_uri = URI.parse('http://deals.mobstaging.com/loot/verify_install.json')
   post_args = {
     'verify[permalink]' => app_permalink,
@@ -29,11 +40,5 @@ end
 
 
 if __FILE__ == $0
-  hashed_mac_address = hash_mac_address('salt', '11:11:11:11:11:11')
-  secret_hash = compute_secret_hash('5bb75e8dd6300cadcdd07fa2c46a3c10', 'lulzio', hashed_mac_address)
-  response, data = report_install_to_crowdmob('lulzio', hashed_mac_address, secret_hash)
-  json = JSON.parse(response.body)
-
-  puts "HTTP status code: #{response.code}"
-  puts "CrowdMob internal status code: #{json['install_status']}"
+  report_install_to_crowdmob
 end

c5739439cbf379a55a30e08c6950f192e65f1cfe
Raj Shah <brainix@gmail.com>
2012-09-25 12:46:40 -0700

    commented source code

--- server.sh
+++ server.sh
@@ -7,8 +7,17 @@
 # you use on your server.
 
 
+# You can test against CrowdMob's staging server located at:
 server_url="http://deals.mobstaging.com/loot/verify_install.json"
+
+# Eventually, you'll want to switch over to CrowdMob's production server
+# located at:
+# server_url="https://deals.crowdmob.com/loot/verify_install.json"
+
+# When you registered your app with CrowdMob, you got a secret key and a
+# permalink:
 app_permalink="lulzio"
+
 uuid="6751f06f9f9b83b9c1e80936ffe59bc536700b2"
 secret_hash="b53cd8652cb1feb3b10a087a6ed97566c3c50544607c9c086d7fbbb3d13f8c00"
 

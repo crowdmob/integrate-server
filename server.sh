8f3cbbbc09b33c9f11a7665f3a7d3aebd3fbd6f7
Raj Shah <brainix@gmail.com>
2012-09-24 17:29:42 -0700

    improved shell script

---
+++ server.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+curl --data "verify[permalink]=lulzio&verify[uuid]=6751f06f9f9b83b9c1e80936ffe59bc536700b2&verify[uuid_type]=publisher_device_id&verify[secret_hash]=b53cd8652cb1feb3b10a087a6ed97566c3c50544607c9c086d7fbbb3d13f8c00" "http://deals.mobstaging.com/loot/verify_install.json"
+echo

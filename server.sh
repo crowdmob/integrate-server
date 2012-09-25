90174de19e27044fc36b2952437a8e72678a2e1a
Raj Shah <brainix@gmail.com>
2012-09-24 17:50:46 -0700

    cleaned up sample server-to-server bash script

--- server.sh
+++ server.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-curl --data "verify[permalink]=lulzio&verify[uuid]=6751f06f9f9b83b9c1e80936ffe59bc536700b2&verify[uuid_type]=publisher_device_id&verify[secret_hash]=b53cd8652cb1feb3b10a087a6ed97566c3c50544607c9c086d7fbbb3d13f8c00" "http://deals.mobstaging.com/loot/verify_install.json"
+
+server_url="http://deals.mobstaging.com/loot/verify_install.json"
+app_permalink="lulzio"
+uuid="6751f06f9f9b83b9c1e80936ffe59bc536700b2"
+secret_hash="b53cd8652cb1feb3b10a087a6ed97566c3c50544607c9c086d7fbbb3d13f8c00"
+
+
+curl --data "verify[permalink]=$app_permalink&verify[uuid]=$uuid&verify[uuid_type]=publisher_device_id&verify[secret_hash]=$secret_hash" $server_url
 echo
